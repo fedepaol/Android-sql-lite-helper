a97769ba5d138c9462be799254266d636b5fea19
fede <fedepaol@gmail.com>
2011-01-18 23:30:50 +0100

    Inheritance modifications. Thanks to smartgaddix@gmail.com

--- sql_lite_helper.py
+++ sql_lite_helper.py
@@ -22,6 +22,9 @@ type_dictionary = { 'String':'text',
                     'Integer':'integer',
                     'Date':'integer'}
 
+constraint_dictionary = { 'NotNull':'not null',
+                          'Unique':'unique'}
+
 class ClassField():
     '''keeps the informations related to a single field of a class'''
     def __init__(self, name, class_name):
@@ -32,15 +35,16 @@ class ClassField():
     def __repr__(self):
         return self.name
 
-importmodules = '''import java.util.Date;
-import android.content.ContentValues;
+importmodules = '''import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
-import android.util.Log;'''
+import android.util.Log;
+
+import java.util.Date;'''
 
 
 banner = '''/**********************************************************************************************************************************************************************
@@ -49,65 +53,70 @@ banner = '''/*******************************************************************
 '''
 
 class_creation = '''public class %s{
+    
+    private static final String TAG = "%s";
+
     private static final String DATABASE_NAME = "%sDb.db";
     private static final int DATABASE_VERSION = 1;'''
 
 generic_methods = '''
     // Variable to hold the database instance
-    private SQLiteDatabase db;
+    protected SQLiteDatabase mDb;
     // Context of the application using the database.
-    private final Context context;
+    private final Context mContext;
     // Database open/upgrade helper
-    private myDbHelper dbHelper;
-    public %s(Context _context) {
-        context = _context;
-        dbHelper = new myDbHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
+    private MyDbHelper mDbHelper;
+    
+    public %s(Context context) {
+        mContext = context;
+        mDbHelper = new MyDbHelper(mContext, DATABASE_NAME, null, DATABASE_VERSION);
     }
     
     public %s open() throws SQLException { 
-        db = dbHelper.getWritableDatabase();
+        mDb = mDbHelper.getWritableDatabase();
         return this;
     }
                                                      
     public void close() {
-        db.close();
+        mDb.close();
     }'''
 
 
 end_stuff ='''
-    private static class myDbHelper extends SQLiteOpenHelper {
-        public myDbHelper(Context context, String name, CursorFactory factory, int version) {
+    private static class MyDbHelper extends SQLiteOpenHelper {
+    
+        public MyDbHelper(Context context, String name, CursorFactory factory, int version) {
             super(context, name, factory, version);
         }
 
         // Called when no database exists in disk and the helper class needs
         // to create a new one. 
         @Override
-        public void onCreate(SQLiteDatabase _db) {      
+        public void onCreate(SQLiteDatabase db) {      
             %s
         }
 
         // Called when there is a database version mismatch meaning that the version
         // of the database on disk needs to be upgraded to the current version.
         @Override
-        public void onUpgrade(SQLiteDatabase _db, int _oldVersion, int _newVersion) {
-          // Log the version upgrade.
-          Log.w("TaskDBAdapter", "Upgrading from version " + 
-                                 _oldVersion + " to " +
-                                 _newVersion + ", which will destroy all old data");
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // Log the version upgrade.
+            Log.w(TAG, "Upgrading from version " + 
+                        oldVersion + " to " +
+                        newVersion + ", which will destroy all old data");
             
-          // Upgrade the existing database to conform to the new version. Multiple 
-          // previous versions can be handled by comparing _oldVersion and _newVersion
-          // values.
+            // Upgrade the existing database to conform to the new version. Multiple 
+            // previous versions can be handled by comparing _oldVersion and _newVersion
+            // values.
 
-          // The simplest case is to drop the old table and create a new one.
-          %s
-          // Create a new one.
-          onCreate(_db);
+            // The simplest case is to drop the old table and create a new one.
+            %s
+            // Create a new one.
+            onCreate(db);
         }
-      }
+    }
      
-      /** Dummy object to allow class to compile */
+    /** Dummy object to allow class to compile */
 }''' 
 
 def add_indentation(function):
@@ -126,12 +135,12 @@ class SqlLiteHelper():
             if values[0] == 'CLASS':
                 self._classes.append(ClassImplementer(file, values[1]))
 
-    def write_dbadapter(self, target_file, name, pckg):
+    def write_dbadapter(self, target_file, name, pckg, dbname):
         target_file.write(banner)
-        target_file.write('package %s;\n'%(pckg))
+        target_file.write('package %s;\n\n'%(pckg))
         target_file.write(importmodules)
         self.write_separators(target_file)
-        target_file.write(class_creation%(name, name))
+        target_file.write(class_creation%(name, name, dbname))
         self.write_separators(target_file)
 
         target_file.write(generic_methods%(name, name))
@@ -211,11 +220,11 @@ class SqlLiteHelper():
         
 
     def get_db_create(self):
-        rows = map(lambda x:'_db.execSQL(%s);\n\t\t'%(x.database_create_name), self._classes)
+        rows = map(lambda x:'db.execSQL(%s);\n\t\t\t'%(x.database_create_name), self._classes)
         return ''.join(rows)
 
     def get_db_drop(self):
-        rows = map(lambda x:'_db.execSQL("DROP TABLE IF EXISTS " + %s + ";");\n\t\t'%(x._table_name), self._classes)
+        rows = map(lambda x:'db.execSQL("DROP TABLE IF EXISTS " + %s + ";");\n\t\t\t'%(x._table_name), self._classes)
         return ''.join(rows)
 
 
@@ -247,38 +256,56 @@ class ClassImplementer():
 
             #TODO Check che type sia un tipo accettabile
             field_name = fields[1]
-            try:
+            '''try:
                 if fields[2] == '*':
                     key = True
                 else:
                     key = False
             except:
                 key = False
+            '''
+            try:
+                constr1 = fields[2]
+            except:
+                constr1 = ''
+                
+            try:
+                constr2 = fields[3]
+            except:
+                constr2 = ''
 
             field = ClassField(field_name, self._name)
             field.type = type
-            field.key = key
+            '''field.key = key'''
             field.sql_lite_type = type_dictionary[type]
+            if constr1 != '':
+                field.sql_lite_constr1 = constraint_dictionary[constr1]
+            else:
+                field.sql_lite_constr1 = ''
+            if constr2 != '':
+                field.sql_lite_constr2 = constraint_dictionary[constr2]
+            else:
+                field.sql_lite_constr2 = ''
             self._class_fields.append(field)
 
     def get_table_create(self):
         '''returns table creation command'''
-        command = 'private static final String %s = "create table " + %s + " (" + %s + " integer primary key autoincrement"'%(self.database_create_name, self._table_name, self._row_id) + ''.join(map(lambda x: '+ ", " + \n\t\t\t\t %s + " %s "'%(x.key_name,  x.sql_lite_type), self._class_fields)) + ' + ");";\n'
+        command = 'private static final String %s = "create table " + %s + " (" + \n\t\t\t\t %s + " integer primary key autoincrement"'%(self.database_create_name, self._table_name, self._row_id) + ''.join(map(lambda x: ' + ", " + \n\t\t\t\t %s + " %s %s %s"'%(x.key_name,  x.sql_lite_type, x.sql_lite_constr1, x.sql_lite_constr2), self._class_fields)) + ' + ");";\n'
 
         return command;
         
     def get_static_declaration_fields(self):
         '''returns a list of declarations of every field needed for sqllite usage'''
         static_fields = []
-        template_static_string = r'private static final String %s = "%s";'
-        template_static_int = r'private static final int %s = %d;'
+        template_static_string = r'public static final String %s = "%s";'
+        template_static_int = r'protected static final int %s = %d;'
         static_fields.append(template_static_string%(self._table_name, self._name))
 
         for column_num, field in enumerate(self._class_fields):
             static_fields.append(template_static_string%(field.key_name, field.name))
             static_fields.append(template_static_int%(field.column_name, column_num + 1))
 
-        static_fields.append(template_static_string%(self._row_id, '_id'))
+        static_fields.append(template_static_string%(self._row_id, '_id'))        
 
         return static_fields
 
@@ -297,51 +324,51 @@ class ClassImplementer():
 
     def get_arg_fields_list(self):
         ''' returns the fields of the class with the given time, separated by , useful in case of function argument list'''
-        return ','.join('%s %s'%(field.type, field.name) for field in self._class_fields)
+        return ', '.join('%s %s'%(field.type, field.name) for field in self._class_fields)
 
 
     def build_update_function(self):
         ''' returns the update function''' 
         function_sign = 'public long update%s('%(self._name)
-        args = 'long _rowIndex, ' + self.get_arg_fields_list()
+        args = 'long rowIndex, ' + self.get_arg_fields_list()
         function_sign = function_sign + args + ')'
 
-        function_body = '\tString where = %s+ " = " + _rowIndex;\n'%(self._row_id) + \
-        self.get_content_values() + '\treturn db.update(%s, contentValues, where, null);\n'%(self._table_name)
+        function_body = '\tString where = %s + " = " + rowIndex;\n'%(self._row_id) + \
+        self.get_content_values() + '\treturn mDb.update(%s, contentValues, where, null);\n'%(self._table_name)
         return self.get_function(function_sign, function_body)
 
     def build_add_function(self):
         function_sign = 'public long add%s('%(self._name)
         function_args = self.get_arg_fields_list()
         function_sign = function_sign + function_args + ')'
-        function_body = self.get_content_values() + '\treturn db.insert(%s, null, contentValues);\n'%(self._table_name)
+        function_body = self.get_content_values() + '\treturn mDb.insert(%s, null, contentValues);\n'%(self._table_name)
         return self.get_function(function_sign, function_body)
 
     def build_remove_function(self):
-        function_sign = 'public boolean remove%s(Long _rowIndex)'%self._name
-        function_body = '\treturn db.delete(%s, %s+ "=" + _rowIndex, null) > 0;'%(self._table_name, self._row_id)
+        function_sign = 'public boolean remove%s(Long rowIndex)'%self._name
+        function_body = '\treturn mDb.delete(%s, %s + " = " + rowIndex, null) > 0;'%(self._table_name, self._row_id)
         return self.get_function(function_sign, function_body)
 
     def build_remove_all_function(self):
-        function_sign = 'public boolean removeAll%s(Long _rowIndex)'%(self._name)
-        function_body = '\treturn db.delete(%s, null, null) > 0;'%(self._table_name)
+        function_sign = 'public boolean removeAll%s()'%(self._name)
+        function_body = '\treturn mDb.delete(%s, null, null) > 0;'%(self._table_name)
         return self.get_function(function_sign, function_body)
 
     def get_keys_array(self):
         ''' returs all the _KEY fields (the names of the columns) separated by , useful when I need to get the results of a cursor'''
-        return self._row_id + ',\n\t\t\t\t' + ',\n\t\t\t\t'.join(field.key_name for field in self._class_fields)
+        return '\n\t\t\t\t' + self._row_id + ',\n\t\t\t\t' + ',\n\t\t\t\t'.join(field.key_name for field in self._class_fields)
 
     def build_get_all_function(self):
         function_sign = 'public Cursor getAll%s()'%(self._name)
         keys = self.get_keys_array()
-        function_body = '\treturn db.query(%s, new String[] {%s}, null, null, null, null, null);'%(self._table_name, keys) 
+        function_body = '\treturn mDb.query(%s, new String[] {%s}, null, null, null, null, null);'%(self._table_name, keys) 
         return self.get_function(function_sign, function_body)
 
     def build_get_one_function(self):
-        function_sign = 'public Cursor get%s(long _rowIndex)'%(self._name)
+        function_sign = 'public Cursor get%s(long rowIndex)'%(self._name)
         keys = self.get_keys_array()
-        where = '%s + " = " + _rowIndex'%(self._row_id)
-        function_body = '\tCursor res = db.query(%s, new String[] {%s}, %s, null, null, null, null);\n'%(self._table_name, keys, where) + '\tif(res != null){\n\t\tres.moveToFirst();\n\t}\n\treturn res;'
+        where = '%s + " = " + rowIndex'%(self._row_id)
+        function_body = '\tCursor res = mDb.query(%s, new String[] {%s}, %s, null, null, null, null);\n'%(self._table_name, keys, where) + '\tif(res != null){\n\t\tres.moveToFirst();\n\t}\n\treturn res;'
         return self.get_function(function_sign, function_body)
 
 
@@ -351,6 +378,7 @@ def parse_options():
     parser.add_option("-i", "--infile", dest="infile", help="file that contains classes definition", default="in.txt")
     parser.add_option("-n", "--name", dest="name", help="name of the dbhelper class ", default="dbhelper")
     parser.add_option("-p", "--package", dest="package", help="name of the package", default="")
+    parser.add_option("-d", "--dbname", dest="dbname", help="name of the database file", default="dbhelper")
     (options, args) = parser.parse_args()
     return options
 
@@ -369,6 +397,6 @@ if __name__ == '__main__':
         infile = open(opt.infile)
         helper = SqlLiteHelper(infile)
         target_file = open('%s.java'%(opt.name), 'w')
-        helper.write_dbadapter(target_file, opt.name, opt.package)
+        helper.write_dbadapter(target_file, opt.name, opt.package, opt.dbname)
         target_file.close()
 
